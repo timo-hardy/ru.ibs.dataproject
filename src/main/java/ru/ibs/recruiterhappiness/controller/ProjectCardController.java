30d778a59fb94fb550505980e1d1436fdcd04d03
Timur <timos.gooogle@gmail.com>
2021-11-23 19:46:54 +0500

    Realization ProjectCardController

--- src/main/java/ru/ibs/recruiterhappiness/controller/ProjectCardController.java
+++ src/main/java/ru/ibs/recruiterhappiness/controller/ProjectCardController.java
@@ -1,13 +1,33 @@
 package ru.ibs.recruiterhappiness.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.ibs.recruiterhappiness.model.ProjectCard;
+import ru.ibs.recruiterhappiness.service.ProjectCardService;
+
+import java.util.List;
 
 /**
  * @author Timur Khidirov on 23.11.2021
  */
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("api/card")
 public class ProjectCardController {
+    private final ProjectCardService projectCardService;
 
+    @GetMapping("read")
+    public List<ProjectCard> getAll() {
+        return projectCardService.findAll();
+    }
+
+    @RequestMapping("create")
+    public void add(@RequestBody ProjectCard projectCard) {
+        projectCardService.add(projectCard);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public void delete(@PathVariable Long id) {
+        projectCardService.delete(id);
+    }
 }
