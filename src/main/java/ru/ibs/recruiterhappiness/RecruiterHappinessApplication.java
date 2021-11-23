ba09848254f3c4ee3c81cb1248e5e0fbd93ddfbb
Timur <timos.gooogle@gmail.com>
2021-11-23 16:27:20 +0500

    git init

---
+++ src/main/java/ru/ibs/recruiterhappiness/RecruiterHappinessApplication.java
@@ -0,0 +1,13 @@
+package ru.ibs.recruiterhappiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruiterHappinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruiterHappinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ibs/recruiterhappiness/RecruiterHappinessApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ibs.recruiterhappiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruiterHappinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
