93bf6dd2750270227b4144a2fece1fd8545be673
Timur <timos.gooogle@gmail.com>
2021-11-23 16:50:52 +0500

    Add model ProjectCard

--- src/main/java/ru/ibs/recruiterhappiness/model/ProjectCard.java
+++ src/main/java/ru/ibs/recruiterhappiness/model/ProjectCard.java
@@ -1,7 +1,23 @@
 package ru.ibs.recruiterhappiness.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * @author Timur Khidirov on 23.11.2021
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class ProjectCard {
+    @Id
+    @GeneratedValue
+    private String customer;
+    private String projectName;
 }
