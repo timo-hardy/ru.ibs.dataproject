55b09f9d2f753ef054e253bb38ba0526f4e84477
timurio <timos.gooogle@gmail.com>
2021-11-25 13:50:19 +0500

    Realization DevelopmentMethodology

--- src/main/java/ru/ibs/dataprojects/model/ProjectCard.java
+++ src/main/java/ru/ibs/dataprojects/model/ProjectCard.java
@@ -1,13 +1,18 @@
 package ru.ibs.dataprojects.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Date;
 
 /**
  * @author Timur Khidirov on 23.11.2021
@@ -22,15 +27,13 @@ public class ProjectCard {
     private long cardId;
     private String customerOfTheProject;
     private String projectName;
-
     @OneToOne(cascade = CascadeType.ALL)
     private TypeProject typeProject;
-
     private String functionalDirection;
     private String subjectAreaOfTheProject;
     private String projectDescription;
     private String tasksOfTheProject;
-
     private ApplicationProjectStage applicationProjectStage;
-
+    private LocalDate projectCompletionDate;
+    private String technologiesUsed;
 }
