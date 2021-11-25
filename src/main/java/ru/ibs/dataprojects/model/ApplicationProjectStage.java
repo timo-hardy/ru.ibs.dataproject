9baa3386abafb000504265b45b2afb5b341c2cc7
timurio <timos.gooogle@gmail.com>
2021-11-25 11:46:47 +0500

    Bug fix in ApplicationProjectStage

--- src/main/java/ru/ibs/dataprojects/model/ApplicationProjectStage.java
+++ src/main/java/ru/ibs/dataprojects/model/ApplicationProjectStage.java
@@ -4,9 +4,10 @@ package ru.ibs.dataprojects.model;
  * @author Timur Khidirov on 24.11.2021
  */
 public enum ApplicationProjectStage {
-    PROJECT_STAGE_INITIALIZATION("Инициализация"),
-    PROJECT_STAGE_START("Начало"),
-    PROJECT_STAGE_MIDDLE("Середина"),
+    PROJECT_STAGE_INITIATION("Инициация"),
+    PROJECT_STAGE_PLANNING("Планирование"),
+    PROJECT_STAGE_PERFOMANCE("Выполнение"),
+    PROJECT_STAGE_MONITORING("Мониторинг"),
     PROJECT_STAGE_END("Завершение");
 
     private final String stage_project;
