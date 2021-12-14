75a470f9364f9246453c9209d4a0cf6b02eb803e
timurio <timos.gooogle@gmail.com>
2021-12-14 13:05:46 +0500

    Added LocalDateConverter.java

---
+++ src/main/java/ru/ibs/dataprojects/config/LocalDateConverter.java
@@ -0,0 +1,27 @@
+package ru.ibs.dataprojects.config;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.Optional;
+
+/**
+ * @author Timur Khidirov on 14.12.2021
+ */
+@Converter(autoApply = true)
+public class LocalDateConverter implements AttributeConverter<LocalDate, Date> {
+    @Override
+    public Date convertToDatabaseColumn(LocalDate localDate) {
+        return Optional.ofNullable(localDate)
+                .map(Date::valueOf)
+                .orElse(null);
+    }
+
+    @Override
+    public LocalDate convertToEntityAttribute(Date dbData) {
+        return Optional.ofNullable(dbData)
+                .map(Date::toLocalDate)
+                .orElse(null);
+    }
+}
